75bc3e6cc27c5ce24f6d78a4dd322418242de0b3
18carica <mocelin.leandro@gmail.com>
2023-12-10 23:27:43 -0300

    Descricoes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// Definindo o nome do herói e sua experiência
 let heroi = "Lord Sith Order of the Bloods";
 let xp = 5001;
+
+// Inicializando a variável de nível
 let nivel = "";
 
+// Verificando o nível com base na experiência do herói
 if (xp < 1000) {
     nivel = "Ferro";
 } else if (xp < 2000) {
@@ -18,4 +22,5 @@ if (xp < 1000) {
     nivel = "Radiante";
 }
 
+// Exibindo o resultado no console
 console.log(`O herói de nome ${heroi} está no nível de ${nivel}`);
